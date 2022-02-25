7f7a0c43bbf807c0687048ffffd098fdc5462ec8
Jeffrey <Jeffrey.2001.2312@gmail.com>
2022-02-25 15:12:25 +0100

    Update

---
+++ PlofPlantWeb/Controllers/KassenController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlofPlantData.Models;
+
+namespace PlofPlantWeb.Controllers
+{
+    public class KassenController : Controller
+    {
+        private readonly ILogger<KassenController> _logger;
+        private readonly PlofPlantDbContext _db;
+
+        public KassenController(ILogger<KassenController> logger, PlofPlantDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Kas(int? kasId)
+        {
+            var model = new Kas();
+            if (kasId.HasValue)
+            {
+                model = _db.Kassen
+                    .Where(m => m.Id == kasId.Value)
+                    .FirstOrDefault();
+            }
+
+            return View(model);
+        }
+    }
+}
